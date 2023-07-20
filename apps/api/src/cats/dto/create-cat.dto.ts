9ed93077a5504608b0d6111f6619a02ccc69b5ec
Dan <danielle.bote@ph.ey.com>
2023-07-20 10:34:41 +0800

    chore(back-end): add class validator for validating api dto

--- apps/api/src/cats/dto/create-cat.dto.ts
+++ apps/api/src/cats/dto/create-cat.dto.ts
@@ -1,5 +1,18 @@
+import { IsNumber, IsString, Max, Min } from 'class-validator';
+
 export class CreateCatDto {
+  @IsString()
   readonly name: string;
+
+  @IsNumber()
+  @Min(1, {
+    message: 'Age should be 1-100',
+  })
+  @Max(100, {
+    message: 'Age should be 1-100',
+  })
   readonly age: number;
+
+  @IsString()
   readonly breed: string;
 }
