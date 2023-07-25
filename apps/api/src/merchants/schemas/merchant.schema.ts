df48608d56582e6aa36b483713342b8d925d858f
Trecia Ann <126449551+TreshAnn@users.noreply.github.com>
2023-07-25 15:13:11 +0800

    chore(back-end): email validator

--- apps/api/src/merchants/schemas/merchant.schema.ts
+++ apps/api/src/merchants/schemas/merchant.schema.ts
@@ -29,7 +29,22 @@ export class Merchant {
   @Prop()
   merchantName: string;
 
-  @Prop()
+  @Prop({
+    validate: {
+      validator: async function (email) {
+        const user = await this.constructor.findOne({ email });
+        if (user) {
+          if (this.id === user.id) {
+            return true;
+          }
+          return false;
+        }
+        return true;
+      },
+      message: (props) => 'The specified email address is already in use.',
+    },
+    required: [true, 'User email required'],
+  })
   email: string;
 
   @Prop()
