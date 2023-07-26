e7a0f85f4710666d38256038f2c3d0689ac957c2
KezTimple <TCIntern.TIMPLE@ph.ey.com>
2023-07-26 09:51:45 +0800

    chore(back-end): add product schema

---
+++ apps/api/src/auth/product/schema/producs.schema.ts
@@ -0,0 +1,34 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type ProductDocument = HydratedDocument<Product>;
+
+@Schema
+export class Image {
+  @Prop
+  ImgURL: string;
+
+  @Prop
+  ImgAttch: HTMLImageElement;
+}
+
+@Schema
+export class Product {
+  @Prop()
+  productID: string;
+
+  @Prop()
+  productImg: Image;
+
+  @Prop()
+  productName: string;
+
+  @Prop()
+  productInfo: string;
+
+  @Prop()
+  productPrice: string;
+
+  @Prop()
+  productInventory: string;
+}
