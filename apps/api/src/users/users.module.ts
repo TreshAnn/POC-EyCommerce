42c6b63b3feaf68775a75f5fb01b862da48c732c
Sandrex Endino <sandrex.cayle.endino@ph.ey.com>
2023-07-26 10:04:26 +0800

    feat(backend): Create User API

--- apps/api/src/users/users.module.ts
+++ apps/api/src/users/users.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { User, UserSchema } from './schemas/user.schema';
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
 })
--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -1,25 +1,42 @@
-import { Injectable } from '@nestjs/common';
-
-// This should be a real class/interface representing a user entity
-export type User = any;
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './schemas/user.schema';
 
 @Injectable()
 export class UsersService {
-  // Test Data
-  private readonly users = [
-    {
-      userId: 1,
-      username: 'dancute',
-      password: 'password',
-    },
-    {
-      userId: 2,
-      username: 'abubadan',
-      password: 'guess',
-    },
-  ];
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+  ) {}
 
-  async findOne(username: string): Promise<User | undefined> {
-    return this.users.find((user) => user.username === username);
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    // Check if Username already exists in the database
+    const usernameAlreadyExists = await this.userModel
+      .findOne({ username: createUserDto.username })
+      .exec();
+
+    if (usernameAlreadyExists) {
+      throw new BadRequestException('Username already exists');
+    }
+
+    // Check if Email already exists in the database
+    const emailAlreadyExists = await this.userModel
+      .findOne({ email: createUserDto.email })
+      .exec();
+
+    if (emailAlreadyExists) {
+      throw new BadRequestException('Email already exists');
+    }
+
+    const createdUser = await this.userModel.create({
+      ...createUserDto,
+      userType: 'consumer',
+    });
+    return createdUser;
+  }
+
+  async findOne(id: string): Promise<User> {
+    return this.userModel.findOne({ _id: id }).exec();
   }
 }
