a5d6296d6458278c37df1a7e764fc42644b47e67
markjoshuacarmen <Mark.Joshua.Carmen@ph.ey.com>
2023-07-26 14:14:02 +0800

    chore: add updateUser service

--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, BadRequestException } from '@nestjs/common';
+import {
+  Injectable,
+  BadRequestException,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -38,5 +42,20 @@ export class UsersService {
 
   async findOne(id: string): Promise<User> {
     return this.userModel.findOne({ _id: id }).exec();
+  }
+  async updateUser(
+    userId: string,
+    CreateUserDto: CreateUserDto,
+  ): Promise<User> {
+    const existingUser = await this.userModel.findById(userId);
+    if (!existingUser) {
+      throw new NotFoundException('User not found');
+    }
+
+    // Update only the fields that are present in the updateUserDto
+    Object.assign(existingUser, CreateUserDto);
+
+    const updatedUser = await existingUser.save();
+    return updatedUser;
   }
 }
