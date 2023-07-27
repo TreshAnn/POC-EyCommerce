533badedec897895f4d86b0033815e0657ad1a60
Sandrex Endino <sandrex.cayle.endino@ph.ey.com>
2023-07-27 10:30:06 +0800

    chore(backend): add auth.service create method

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,22 +1,33 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
-import { UsersService } from '../users/users.service';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CreateAuthDto } from './dto/auth.dto';
+import { Auth, AuthDocument } from './schema/auth.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class AuthService {
   constructor(
-    private usersService: UsersService,
-    private jwtService: JwtService,
+    @InjectModel(Auth.name) private readonly authModel: Model<AuthDocument>,
   ) {}
 
-  async signIn(username, pass) {
-    //   const user = await this.usersService.findOne(username);
-    //   if (user?.password !== pass) {
-    //     throw new UnauthorizedException();
-    //   }
-    //   const payload = { username: user.username, sub: user.userId };
-    //   return {
-    //     access_token: await this.jwtService.signAsync(payload),
-    //   };
+  async create(createAuthDto: CreateAuthDto): Promise<Auth> {
+    const usernameAlreadyExists = await this.authModel
+      .findOne({ username: createAuthDto.username })
+      .exec();
+
+    if (usernameAlreadyExists) {
+      throw new BadRequestException('Username already exists');
+    }
+
+    const emailAlreadyExists = await this.authModel
+      .findOne({ email: createAuthDto.email })
+      .exec();
+
+    if (emailAlreadyExists) {
+      throw new BadRequestException('Email already exists');
+    }
+
+    const createdAuth = await this.authModel.create(createAuthDto);
+    return createdAuth;
   }
 }
