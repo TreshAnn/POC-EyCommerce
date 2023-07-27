e151e90fbd706d93c7d84fff553c6468c0b1adad
Sandrex Endino <sandrex.cayle.endino@ph.ey.com>
2023-07-27 10:36:34 +0800

    chore(backend): make UsersModule forwardRef to AuthModule

--- apps/api/src/users/users.module.ts
+++ apps/api/src/users/users.module.ts
@@ -1,12 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User, UserSchema } from './schemas/user.schema';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    forwardRef(() => AuthModule),
   ],
   controllers: [UsersController],
   providers: [UsersService],
