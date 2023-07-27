5dc99b26c8727a229651a76cd1c9315315decd8d
SGVGen <Genesis.Fragas@ph.ey.com>
2023-07-27 18:50:33 +0800

    feat: Added Footer

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button, Checkbox, HeaderNavBar } from "ui";
+import { Button, Checkbox, Footer, HeaderNavBar } from "ui";
 import "./style.css";
 
 export default function Page() {
@@ -9,6 +9,7 @@ export default function Page() {
       <HeaderNavBar />
       <Button style={{ marginBottom: "10px" }}>Sample</Button>
       <Checkbox label="checkbox 1" />
+      <Footer />
     </>
   );
 }
