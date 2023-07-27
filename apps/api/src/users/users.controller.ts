e04cd1e29db43f6a5bad1804b6cb4cdb302d2b92
brentmillen <brent.millen.manzo@ph.ey.com>
2023-07-28 06:43:23 +0800

    fix(back end): add success message for delete

--- apps/api/src/users/users.controller.ts
+++ apps/api/src/users/users.controller.ts
@@ -36,7 +36,7 @@ export class UsersController {
   @HttpCode(HttpStatus.OK)
   @Delete(':id')
   async remove(@Param('id') userId: string) {
-    return this.userService.delete(userId);
-    // return 'User deleted';
+    await this.userService.delete(userId);
+    return 'User deleted';
   }
 }
