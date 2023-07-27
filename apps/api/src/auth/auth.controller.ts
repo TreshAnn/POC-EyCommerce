d2e4437e008e2f8b567303cbb9d4a6729887417c
Sandrex Endino <sandrex.cayle.endino@ph.ey.com>
2023-07-27 10:40:24 +0800

    chore(backend): add http post method

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -9,20 +9,21 @@ import {
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Public } from './decorators/public.decorator';
+import { CreateAuthDto } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @Public()
-  @HttpCode(HttpStatus.OK)
-  @Post('login')
-  signIn(@Body() signInDto: Record<string, any>) {
-    return this.authService.signIn(signInDto.username, signInDto.password);
-  }
-
   @Get('profile')
   getProfile(@Request() req) {
     return req.user;
+  }
+
+  @Public()
+  @HttpCode(HttpStatus.CREATED)
+  @Post('create')
+  async create(@Body() createAuthDto: CreateAuthDto) {
+    await this.authService.create(createAuthDto);
   }
 }
