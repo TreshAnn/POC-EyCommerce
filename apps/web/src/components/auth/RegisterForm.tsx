b6c13070a5cc3a189d8dbe26b2723227db892ee0
SGVGen <Genesis.Fragas@ph.ey.com>
2023-08-01 16:41:20 +0800

    chore: register form

---
+++ apps/web/src/components/auth/RegisterForm.tsx
@@ -0,0 +1,140 @@
+import {
+  Anchor,
+  Button,
+  Paper,
+  PasswordInput,
+  Stack,
+  Text,
+  TextInput,
+  Input,
+  Group,
+  Flex,
+  Title,
+  Select,
+} from '@mantine/core';
+import React, { useState } from 'react';
+import { Form } from 'ui';
+import * as z from 'zod';
+
+import { StyledContainer } from './styles';
+
+const schema = z.object({
+  email: z.string().min(1, 'Required'),
+  password: z.string().min(8, 'Minimum Password is 8'),
+});
+
+type RegisterValues = {
+  usertype: string;
+  username: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  street: string;
+  region: string;
+  zipcode: number;
+  country: string;
+  password: string;
+  confirmpassword: string;
+};
+
+type RegisterFormProps = {
+  onSuccess: () => void;
+};
+
+export const RegisterForm = ({ onSuccess }: RegisterFormProps) => {
+  const [data, setData] = useState([
+    { value: 'consumer', label: 'Consumer' },
+    { value: 'merchant', label: 'Merchant' },
+  ]);
+
+  return (
+    <StyledContainer>
+      <Paper radius="md" p="xl" withBorder>
+        <Form<RegisterValues, typeof schema>
+          onSubmit={async () => {
+            onSuccess();
+          }}
+          schema={schema}
+        >
+          {({ formState }) => (
+            <>
+              <Stack>
+                <Title order={1}>Register</Title>
+                <div style={{ display: 'none' }}>
+                  <Select
+                    value={data.values[0]}
+                    label="Creatable Select"
+                    data={data}
+                    placeholder="Select items"
+                    nothingFound="Nothing found"
+                    searchable
+                  />
+                </div>
+                <TextInput
+                  label="User Name"
+                  placeholder="Enter your User Name..."
+                  required
+                />
+                <Group grow>
+                  <TextInput
+                    label="First Name"
+                    placeholder="Enter your First Name"
+                    required
+                  />
+                  <TextInput
+                    label="Last Name"
+                    placeholder="Enter your Last Name"
+                    required
+                  />
+                </Group>
+                <TextInput
+                  label="Email"
+                  placeholder="Enter your Email"
+                  required
+                />
+                <TextInput
+                  label="Street Address"
+                  placeholder="Enter your Street"
+                  required
+                />
+                <Group grow>
+                  <TextInput
+                    label="Region"
+                    placeholder="Enter your Region"
+                    required
+                  />
+                  <TextInput
+                    label="ZipCode"
+                    placeholder="Enter your ZipCode"
+                    required
+                  />
+                  <TextInput
+                    label="Country"
+                    placeholder="Enter your Country"
+                    required
+                  />
+                </Group>
+                <PasswordInput
+                  label="Password"
+                  placeholder="Enter your password"
+                  required
+                />
+                <PasswordInput
+                  label="Confirm Password"
+                  placeholder="Confirm your password"
+                  required
+                />
+                <Button color="yellow" variant="filled" type="submit">
+                  Register
+                </Button>
+                <Text>
+                  Already have an account? <Anchor href="#"></Anchor>
+                </Text>
+              </Stack>
+            </>
+          )}
+        </Form>
+      </Paper>
+    </StyledContainer>
+  );
+};
