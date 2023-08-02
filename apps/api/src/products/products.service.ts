fbebc7c5524ee6deadc985b84749c9dac8650f56
markjoshuacarmen <Mark.Joshua.Carmen@ph.ey.com>
2023-08-02 11:09:31 +0800

    chore(backend): add read product API

--- apps/api/src/products/products.service.ts
+++ apps/api/src/products/products.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateProductDto } from '../products/dto/create-product.dto';
@@ -11,7 +15,25 @@ export class ProductsService {
   ) {}
 
   async create(createProductDto: CreateProductDto): Promise<Product> {
+    const productAlreadyExists = await this.productModel
+      .findOne({ productID: createProductDto.productID })
+      .exec();
+    if (productAlreadyExists) {
+      throw new BadRequestException('Product ID already exists');
+    }
     const createdProduct = await this.productModel.create(createProductDto);
     return createdProduct;
+  }
+
+  async findOne(id: string): Promise<Product> {
+    const merchant = await this.productModel.findOne({ _id: id });
+
+    if (!merchant) {
+      throw new NotFoundException('Merchant not found');
+    }
+
+    //console.log(merchant);
+
+    return merchant;
   }
 }
