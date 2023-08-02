4943f6ffe555b2020b33ef9217534fa058429578
Trecia Ann <126449551+TreshAnn@users.noreply.github.com>
2023-08-02 09:05:05 +0800

    chore(backend): add custom validation for address and phoneNumber

---
+++ apps/api/src/utils/custom-validations.utils.ts
@@ -0,0 +1,50 @@
+import { registerDecorator, ValidationOptions, isPhoneNumber as isPhoneNumberValidator } from 'class-validator';
+
+function hasLeadingTrailingSpacesValidator(value: any): boolean {
+  if (typeof value !== 'string') {
+    return false;
+  }
+  const trimmedValue = value.trim();
+  return trimmedValue === value;
+}
+
+export function AddressHasLeadingTrailingSpaces(): PropertyDecorator {
+  return function (object: Record<string, any>, propertyName: string) {
+    registerDecorator({
+      name: 'addressHasLeadingTrailingSpaces',
+      target: object.constructor,
+      propertyName: propertyName,
+      validator: {
+        validate(value: any): boolean {
+          return hasLeadingTrailingSpacesValidator(value);
+        },
+        defaultMessage(): string {
+          return `${propertyName} must not have leading or trailing spaces.`;
+        },
+      },
+    });
+  };
+}
+
+export function isPhoneNumberWithTrim(options?: ValidationOptions): PropertyDecorator {
+  return function (object: Record<string, any>, propertyName: string) {
+    registerDecorator({
+      name: 'isPhoneNumberWithTrim',
+      target: object.constructor,
+      propertyName: propertyName,
+      options: options,
+      validator: {
+        validate(value: any): boolean {
+          if (typeof value !== 'string') {
+            return false;
+          }
+          const trimmedValue = value.trim();
+          return isPhoneNumberValidator(trimmedValue, undefined) && trimmedValue === value;
+        },
+        defaultMessage(): string {
+          return 'Invalid phone number format. Phone number must not have leading or trailing spaces and should be a valid phone number.';
+        },
+      },
+    });
+  };
+}
