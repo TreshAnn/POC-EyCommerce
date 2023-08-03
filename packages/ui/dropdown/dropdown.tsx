c94a69f9e6ab15007e5fbd1fe3314cb28fda77cd
Trecia Ann <126449551+TreshAnn@users.noreply.github.com>
2023-08-03 13:26:44 +0800

    fix(frontend): modify dropdown component

--- packages/ui/dropdown/dropdown.tsx
+++ packages/ui/dropdown/dropdown.tsx
@@ -1,52 +1,34 @@
-import { Avatar, Menu, Text } from '@mantine/core';
-import { Link } from 'react-router-dom';
+import { Menu } from '@mantine/core';
 
 interface DropdownProps {
   target: React.ReactNode;
+  menuItems: (string | 'divider')[];
+  avatarContent?: React.ReactNode;
 }
 
-export const Dropdown: React.FC<DropdownProps> = ({ target }) => {
+export const Dropdown: React.FC<DropdownProps> = ({
+  target,
+  menuItems,
+  avatarContent,
+}) => {
   return (
     <Menu shadow="md" width={200} position="bottom-start">
       <Menu.Target>{target}</Menu.Target>
 
       <Menu.Dropdown>
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            gap: 10,
-            paddingLeft: 8,
-            paddingTop: 2,
-          }}
-        >
-          <Avatar
-            size="md"
-            src="/vite.svg"
-            alt="User Photo"
-            style={{ borderRadius: '50%', border: '1px solid #000' }}
-          />
-          <div>
-            <div style={{ flex: 1 }}>
-              <Text size="sm" weight={500}>
-                Olivia Rhye
-              </Text>
-
-              <Text color="dimmed" size="xs">
-                beta@email.com
-              </Text>
-            </div>
+        {avatarContent && (
+          <div style={{ padding: '8px 10px', minWidth: 200 }}>
+            {avatarContent}
           </div>
-        </div>
-
+        )}
         <Menu.Divider />
-        <Menu.Item>View Profile</Menu.Item>
-        <Menu.Item>Settings</Menu.Item>
-        <Menu.Divider />
-        <Menu.Item>My Orders</Menu.Item>
-        <Menu.Item>My Wallet</Menu.Item>
-        <Menu.Divider />
-        <Menu.Item> Sign Out</Menu.Item>
+        {menuItems.map((item, index) => {
+          if (item === 'divider') {
+            return <Menu.Divider key={index} />;
+          } else {
+            return <Menu.Item key={index}>{item}</Menu.Item>;
+          }
+        })}
       </Menu.Dropdown>
     </Menu>
   );
