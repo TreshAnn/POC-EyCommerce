a0e76097dd1431d642bf9ba60de8c65c36c5fc5a
Sandrex Endino <sandrex.cayle.endino@ph.ey.com>
2023-08-04 12:10:55 +0800

    chore(frontend): remove unused component in SampleView

--- apps/web/src/views/SampleView.tsx
+++ apps/web/src/views/SampleView.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Dropdown } from '../../../../packages/ui/dropdown/dropdown';
 import { Searchbar } from '../../../../packages/ui/searchbar/searchbar';
+
 export const SampleView = () => {
   return (
     <>
-      {/* <Dropdown /> */}
       <Searchbar />
     </>
   );
