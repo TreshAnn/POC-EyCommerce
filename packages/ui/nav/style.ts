742117d1864c3ee8af65341e4e06e8d2153c29a8
SGVGen <Genesis.Fragas@ph.ey.com>
2023-08-04 14:32:12 +0800

    fix: import error for Header

--- packages/ui/nav/style.ts
+++ packages/ui/nav/style.ts
@@ -1,13 +1,16 @@
 import styled from 'styled-components';
-import { Box } from '@mantine/core';
 
-export const StyledBox = styled(Box)`
-  background: #ffc815;
-  padding: 50px 40px;
+export const AvatarContainer = styled.div`
+  justify-content: flex-start;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  padding-left: 8px;
+  padding-top: 2px;
 `;
 
-export const StyledAnchor = styled('a')`
-  text-decoration: none;
-  color: #000;
-  font-size: 1.25rem;
+export const CartTextWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
