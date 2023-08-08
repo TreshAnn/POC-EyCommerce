1cf8a7fdc7b06ac873f4c4f2c25707b58afac530
brentmillen <brent.millen.manzo@ph.ey.com>
2023-08-08 09:56:37 +0800

    chore(back-end): remove other fields in cart schema

--- apps/api/src/cart/schemas/cart.schema.ts
+++ apps/api/src/cart/schemas/cart.schema.ts
@@ -1,12 +1,15 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
-import { Product, ProductSchema } from '../../products/schemas/products.schema';
+import { HydratedDocument, SchemaType, SchemaTypes } from 'mongoose';
+import { User } from 'src/users/schemas/user.schema';
+// import { Product, ProductSchema } from '../../products/schemas/products.schema'; - TEST
 
 export type CartDocument = HydratedDocument<Cart>;
 
+export type ItemDocument = HydratedDocument<Item>;
+
 @Schema()
-export class Items {
-  @Prop({ type: ProductSchema, ref: Product.name })
+export class Item {
+  @Prop({ type: SchemaTypes.ObjectId, ref: 'Product' })
   productID: string;
 
   @Prop()
@@ -20,24 +23,29 @@ export class Items {
 
   @Prop()
   quantity: number;
-}
 
+  @Prop()
+  subTotalPrice: number;
+}
 @Schema()
 export class Cart {
-  @Prop()
-  orderDate: string;
+  // @Prop() - TO DO remove and transfer to order/transaction schema (to be created)
+  // orderDate: string;
+
+  // @Prop() - TO DO remove and transfer to order/transaction schema (to be created)
+  // orderStatus: string;
+
+  @Prop({ type: SchemaTypes.ObjectId, ref: User.name })
+  userID: string;
 
   @Prop()
-  orderStatus: string;
-
-  @Prop()
-  orderedItems: Items[];
+  orderedItems: Item[];
 
   @Prop()
   totalAmount: number;
 
-  @Prop()
-  paymentDetails: string;
+  // @Prop() - TO DO remove and transfer to order/transaction schema (to be created)
+  // paymentDetails: string;
 }
 
 export const CartSchema = SchemaFactory.createForClass(Cart);
