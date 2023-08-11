2322ce339b566ab8cddffb7b0c4a560e373af60f
Trecia Ann <126449551+TreshAnn@users.noreply.github.com>
2023-08-11 11:47:49 +0800

    chore(fe): import product and searchbar component

--- apps/web/src/views/SampleView.tsx
+++ apps/web/src/views/SampleView.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 
+import { Product } from '../../../../packages/ui/product/Product';
+import { Searchbar } from '../../../../packages/ui/searchbar/searchbar';
+
 export const SampleView = () => {
   return (
     <main>
       {/* Section height is for demo purposes only; please position the footer at the bottom instead of the middle */}
       <section style={{ height: '100vh' }}>
-        <h1>Hello world!</h1>
-        <h1>Ey React App Boilerplate made by DanDalanDone! TEST HERE</h1>
         <div>
           <br />
+          <Searchbar />
+          <Product />
         </div>
       </section>
     </main>
