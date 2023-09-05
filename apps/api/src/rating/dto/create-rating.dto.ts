c4ed025aa736267db173e3d9fed316f5aff9723b
Sandrex Endino <sandrex.cayle.endino@ph.ey.com>
2023-09-05 14:00:15 +0800

    chore(backend): add createRatingDto for Rating API

---
+++ apps/api/src/rating/dto/create-rating.dto.ts
@@ -0,0 +1,20 @@
+import { Type } from 'class-transformer';
+import { IsNotEmpty, IsNumber, IsString, Max, Min } from 'class-validator';
+
+export class CreateRatingDto {
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(1, {
+    message: 'Rating should be from 1 to 5',
+  })
+  @Max(5, {
+    message: 'Rating should be from 1 to 5',
+  })
+  readonly rating: number;
+
+  @IsString()
+  readonly title: string;
+
+  @IsString()
+  readonly description: string;
+}
