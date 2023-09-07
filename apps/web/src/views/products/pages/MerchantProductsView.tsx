201878f2533604cb80ae017dccce80f9e4188aaa
brentmillen <brent.millen.manzo@ph.ey.com>
2023-09-07 16:30:30 +0800

    chore: remove manual refetch

--- apps/web/src/views/products/pages/MerchantProductsView.tsx
+++ apps/web/src/views/products/pages/MerchantProductsView.tsx
@@ -34,7 +34,6 @@ export const MerchantProducts: React.FC = () => {
   useEffect(() => {
     if (updateProductMutation.isSuccess) {
       setIsModalOpen(false);
-      merchantProductsQuery.refetch();
     }
     if (createProductMutation.isSuccess) {
       handleCloseModal();
