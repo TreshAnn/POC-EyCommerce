75c7bdfa8ad02fe322906f1135d8b1a6bbf9254b
Trecia Ann <126449551+TreshAnn@users.noreply.github.com>
2023-09-07 13:49:58 +0800

    chore(backend): add updateAuth API

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -4,11 +4,13 @@ import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { Auth, AuthSchema } from '../auth/schemas/auth.schema';
+import { MerchantsModule } from 'src/merchants/merchants.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Auth.name, schema: AuthSchema }]),
     forwardRef(() => UsersModule),
+    forwardRef(() => MerchantsModule),
   ],
   providers: [AuthService],
   controllers: [AuthController],
