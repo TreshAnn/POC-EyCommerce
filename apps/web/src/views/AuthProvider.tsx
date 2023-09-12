88ddfa19b8d3154b0b7970e8dbdef0e9f6c4e05d
brentmillen <brent.millen.manzo@ph.ey.com>
2023-09-12 12:26:47 +0800

    chore: setup authprovider for login checking

---
+++ apps/web/src/views/AuthProvider.tsx
@@ -0,0 +1,46 @@
+// AuthProvider.tsx
+import React, {
+  createContext,
+  useContext,
+  useState,
+  ReactNode,
+  useEffect,
+} from 'react';
+import { decodeJwt, JWTPayload } from 'jose';
+
+interface AuthContextType {
+  user: any | null;
+  //   login: (user: User) => void;
+  //   logout: () => void;
+}
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+  const [user, setUser] = useState<JWTPayload | null>(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem('ey_commerce_token');
+    if (token) {
+      // Decode the token to get the user payload
+      const userPayload = decodeJwt(token);
+      setUser(userPayload);
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+};
--- apps/web/src/views/RootView.tsx
+++ apps/web/src/views/RootView.tsx
@@ -1,6 +1,7 @@
 import { MantineProvider } from '@mantine/core';
 import { Notifications } from '@mantine/notifications';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useAuth } from './AuthProvider';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Footer } from 'ui/nav/Footer';
@@ -21,7 +22,10 @@ import { StyledContainer } from './styles/styles';
 
 export const RootView = () => {
   const [queryClient] = React.useState(() => new QueryClient());
+  const { user } = useAuth();
 
+  // eslint-disable-next-line no-console
+  console.log(user);
   return (
     <QueryClientProvider client={queryClient}>
       <MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
@@ -40,10 +44,20 @@ export const RootView = () => {
                 path="/products/:productID"
                 element={<ProductDetailView />}
               />
-              <Route
+              {/* <Route
                 path="/:merchantID/products"
                 element={<MerchantProducts />}
-              />
+              /> */}
+
+              {/* Conditional rendering of protected routes */}
+              {user ? (
+                <>
+                  <Route
+                    path="/:merchantID/products"
+                    element={<MerchantProducts />}
+                  />
+                </>
+              ) : null}
             </Routes>
           </BrowserRouter>
         </StyledContainer>
