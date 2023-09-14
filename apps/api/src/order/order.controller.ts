a54e10d82d0105ba8cd42eb4fdb103cff0cc420e
SGVGen <127918090+SGVGen@users.noreply.github.com>
2023-09-14 19:18:30 +0800

    feat: Auth Gaurd Implementation

--- apps/api/src/order/order.controller.ts
+++ apps/api/src/order/order.controller.ts
@@ -8,11 +8,19 @@ import {
   Get,
   Param,
   Request,
+  UseGuards,
 } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { JwtService } from '@nestjs/jwt';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { Order } from './schemas/order.schema';
+import { CheckAbilities } from 'src/auth/ability/ability.decorator';
+import { AbilityGuard } from 'src/auth/ability/ability.guard';
+import { Action } from 'src/auth/ability/enum/ability.enum';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { Roles } from 'src/auth/decorators/role.decorator';
+import { RolesGuard } from 'src/guards/Role.guard';
+import { Role } from 'src/guards/enum/role.enum';
 
 @Controller('order')
 export class OrderController {
@@ -21,12 +29,21 @@ export class OrderController {
     private jwtService: JwtService,
   ) {}
 
-  @Get('get/:id')
-  @HttpCode(HttpStatus.OK)
-  async findOne(@Param('id') id: string): Promise<Order> {
-    return this.orderService.findOne(id);
+  @UseGuards(AuthGuard)
+  @Roles(Role.CONSUMER)
+  @UseGuards(RolesGuard)
+  @CheckAbilities({ action: Action.Read, subject: Order })
+  @UseGuards(AbilityGuard)
+  @Get('/')
+  async findOne(@Request() req): Promise<Order> {
+    return this.orderService.findOne(req.id);
   }
 
+  @UseGuards(AuthGuard)
+  @Roles(Role.CONSUMER)
+  @UseGuards(RolesGuard)
+  @CheckAbilities({ action: Action.Read, subject: Order })
+  @UseGuards(AbilityGuard)
   @Get('get-all-orders')
   async findAllOrders(@Request() req): Promise<Order[]> {
     return this.orderService.findAllOrders(req);
