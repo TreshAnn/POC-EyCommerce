f191577f9967a57f7060d13377648ab21e6bb6d4
ianchinjen <129832287+ianchinjen@users.noreply.github.com>
2023-09-15 10:43:03 +0800

    Change connection string for db atlas

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -17,11 +17,13 @@ import { OrderModule } from './order/order.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/test'),
+    MongooseModule.forRoot(
+      'mongodb+srv://user:aZkh7DhYjrP2i52U@personalproject.fzwyrwp.mongodb.net/POC-EYCommerce',
+    ),
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '1d' }, // TODO move this to .env
+      signOptions: { expiresIn: '300s' }, // TODO move this to .env
     }),
     CatsModule,
     AuthModule,
