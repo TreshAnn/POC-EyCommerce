3d6ca6ac6fdcd33cd9f79047512c3fa2d8c9b661
brentmillen <brent.millen.manzo@ph.ey.com>
2023-09-20 10:05:41 +0800

    chore: removed test component

--- apps/web/src/views/SampleView.tsx
+++ apps/web/src/views/SampleView.tsx
@@ -1,25 +1,9 @@
-import React, { useState } from 'react';
-import { RatingModal } from 'ui/rating/rating-modal/RatingModal';
-import { Button } from '@mantine/core';
+import React from 'react';
+//add imports for component to test
 
 // NOTE: This view is for testing of component/views(frontend only tasks)
 // When used, please remove imports and components inside main before merging active PR
 export const SampleView = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
-  const handleOpenModal = () => {
-    setIsModalOpen(true);
-  };
-
-  return (
-    <main>
-      <Button ml={40} onClick={handleOpenModal}>
-        Rate Me
-      </Button>
-      <RatingModal isOpen={isModalOpen} onClose={handleCloseModal} />
-    </main>
-  );
+  //Add test functions here
+  return <main>Enter test component here</main>;
 };
