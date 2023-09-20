7c75beed76b668ca6d2a996437a25d1c5c02e8c3
Sandrex Endino <sandrex.cayle.endino@ph.ey.com>
2023-09-21 00:02:05 +0800

    chore(frontend): render TransactionHistory page to SampleView for testing

--- apps/web/src/views/SampleView.tsx
+++ apps/web/src/views/SampleView.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+
 //add imports for component to test
+import { CartData } from '../../../../packages/ui/tansaction-history/sampledata';
+import { TransactionHistory } from '../../../../packages/ui/tansaction-history/transactionPage';
 
 // NOTE: This view is for testing of component/views(frontend only tasks)
 // When used, please remove imports and components inside main before merging active PR
 export const SampleView = () => {
   //Add test functions here
-  return <main>Enter test component here</main>;
+  return (
+    <main>
+      <TransactionHistory
+        merchantName={CartData.merchantName}
+        items={CartData.items}
+        productImg={CartData.productImg}
+        productName={CartData.productName}
+        date={CartData.date}
+        totalAmount={CartData.totalAmount}
+      />
+    </main>
+  );
 };
