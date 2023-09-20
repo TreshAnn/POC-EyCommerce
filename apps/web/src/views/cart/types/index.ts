0f1bba40925d60d5fd250fcc9cef5577cb98996f
brentmillen <brent.millen.manzo@ph.ey.com>
2023-09-20 18:22:30 +0800

    chore: ID to Id to reference backend

--- apps/web/src/views/cart/types/index.ts
+++ apps/web/src/views/cart/types/index.ts
@@ -2,7 +2,7 @@ export interface Cart {
   reduce(arg0: (acc: any, item: any) => any, arg1: number): unknown;
   filter(arg0: (cartItem: any) => boolean): unknown;
   _id: string;
-  userID: string;
+  userId: string;
   orderedItems: OrderedItems[];
   totalAmount: number;
   __v: number;
