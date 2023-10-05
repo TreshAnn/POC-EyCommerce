2be528c472de2f52ee2296cedaacfd8235437232
Sandrex Endino <sandrex.cayle.endino@ph.ey.com>
2023-10-05 16:13:40 +0800

    modify db connection to local db

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -18,9 +18,7 @@ import { RatingModule } from './rating/rating.module';
   imports: [
     //use for local db test: mongodb://localhost:27017/test
     //use for central db test: mongodb+srv://user:aZkh7DhYjrP2i52U@personalproject.fzwyrwp.mongodb.net/POC-EYCommerce
-    MongooseModule.forRoot(
-      'mongodb+srv://user:aZkh7DhYjrP2i52U@personalproject.fzwyrwp.mongodb.net/POC-EYCommerce',
-    ),
+    MongooseModule.forRoot('mongodb://localhost:27017/test'),
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
